df212ff48094161ab8dd5d1f884874f731a6e848
gregsam <gregoryman@windowslive.com>
2019-04-10 11:43:55 +0300

    testing understanding

---
+++ src/STPopulatingVisitor.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import visitor.*;
+
+
+public class STPopulatingVisitor extends GJNoArguDepthFirst<Void>{
+
+    static Map m= new HashMap();
+
+    /**
+     * Grammar production:
+     * f0 -> MainClass()
+     * f1 -> ( TypeDeclaration() )*
+     * f2 -> <EOF>
+     */
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
